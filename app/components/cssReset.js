5ed73189aed76f815b3dff188f79deb465cf05cc
Ben Smithett <bensmithett@users.noreply.github.com>
2020-08-01 14:58:34 +1000

    Better box-sizing

--- app/components/cssReset.js
+++ app/components/cssReset.js
@@ -1,14 +1,15 @@
 export default function cssReset (felaRenderer) {
   felaRenderer.renderStatic({
     boxSizing: 'border-box',
-  }, '*')
-
-  felaRenderer.renderStatic({
     fontSize: '16px',
     fontFamily: 'sans-serif',
     lineHeight: 1.5,
     margin: 0
   }, ':root')
+
+  felaRenderer.renderStatic({
+    boxSizing: 'inherit',
+  }, '*, *:before, *:after')
 
   felaRenderer.renderStatic({
     margin: 0
