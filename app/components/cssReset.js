37bd1744483b82a13f0325c788a2a6d9bbb6c2be
Ben Smithett <bensmithett@users.noreply.github.com>
2020-08-01 14:49:33 +1000

    Support syntax highlighting and add additional CSS reset rules

--- app/components/cssReset.js
+++ app/components/cssReset.js
@@ -1,8 +1,16 @@
 export default function cssReset (felaRenderer) {
   felaRenderer.renderStatic({
+    boxSizing: 'border-box',
+  }, '*')
+
+  felaRenderer.renderStatic({
     fontSize: '16px',
     fontFamily: 'sans-serif',
     lineHeight: 1.5,
+    margin: 0
+  }, ':root')
+
+  felaRenderer.renderStatic({
     margin: 0
   }, 'body')
 }
