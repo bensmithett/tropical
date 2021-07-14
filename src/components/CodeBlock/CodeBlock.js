eebb17f49b07053957a2bd5433df86183e319d94
Ben Smithett <bensmithett@users.noreply.github.com>
2021-07-14 18:01:47 +1000

    Minor tweaks to CodeBlock and cssReset

--- src/components/CodeBlock/CodeBlock.js
+++ src/components/CodeBlock/CodeBlock.js
@@ -7,7 +7,7 @@ export default function CodeBlock({ children, className }) {
   return (
     <Highlight {...defaultProps} code={children.trim()} language={language}>
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
-        <pre className={className} style={{ ...style, padding: '20px' }}>
+        <pre className={className} style={{ ...style, padding: '20px', fontSize: '0.9rem', overflow: 'auto' }}>
           {tokens.map((line, i) => (
             <div key={i} {...getLineProps({ line, key: i })}>
               {line.map((token, key) => (
