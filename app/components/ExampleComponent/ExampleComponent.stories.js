9b93f5c38f50bb7bb4535d979caf3b3106003166
Ben Smithett <bensmithett@users.noreply.github.com>
2021-07-14 18:00:25 +1000

    Move the existing CodeBlock & cssReset into src, delete everything else
    We'll keep using both CodeBlock and cssReset with some minor changes

--- app/components/ExampleComponent/ExampleComponent.stories.js
+++
@@ -1,6 +0,0 @@
-import React from 'react'
-import ExampleComponent from './ExampleComponent'
-
-export default { title: 'ExampleComponent' }
-
-export const basic = () => <ExampleComponent alertMessage={'Hello!'} />
