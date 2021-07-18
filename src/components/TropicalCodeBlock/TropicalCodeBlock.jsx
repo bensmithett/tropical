d32e7de4242009c59ea71224f12b833efc95a35b
Ben Smithett <bensmithett@users.noreply.github.com>
2021-07-18 11:28:00 +1000

    Default to dracula Prism theme and add instructions for adding highlighted languages

--- src/components/TropicalCodeBlock/TropicalCodeBlock.jsx
+++ src/components/TropicalCodeBlock/TropicalCodeBlock.jsx
@@ -1,11 +1,21 @@
 import React from 'react'
 import Highlight, { defaultProps } from 'prism-react-renderer'
+import dracula from 'prism-react-renderer/themes/dracula'
+
+/* 
+To add language support outside the subset that comes with prism-react-renderer
+https://github.com/FormidableLabs/prism-react-renderer/blob/master/src/vendor/prism/includeLangs.js
+
+import Prism from 'prism-react-renderer/prism';
+;(typeof global !== 'undefined' ? global : window).Prism = Prism
+import 'prismjs/components/prism-ruby'
+*/
 
 export function TropicalCodeBlock({ children, className }) {
   const language = className?.replace(/language-/, '')
 
   return (
-    <Highlight {...defaultProps} code={children.trim()} language={language}>
+    <Highlight {...defaultProps} code={children.trim()} language={language} theme={dracula}>
       {({ className, style, tokens, getLineProps, getTokenProps }) => (
         <pre className={className} style={{ ...style, padding: '20px', fontSize: '0.9rem', overflow: 'auto' }}>
           {tokens.map((line, i) => (
