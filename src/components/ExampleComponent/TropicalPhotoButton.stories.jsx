313331d6c6ab201f70a62a67d185d8a76e94750d
Ben Smithett <bensmithett@users.noreply.github.com>
2022-07-31 16:55:11 +1000

    Set up ladle

---
+++ src/components/ExampleComponent/TropicalPhotoButton.stories.jsx
@@ -0,0 +1,5 @@
+import { TropicalPhotoButton } from './TropicalPhotoButton'
+
+export default { title: 'ExampleComponent' }
+
+export const Basic = () => <TropicalPhotoButton alertMessage='Hello!'>Click me!</TropicalPhotoButton>
