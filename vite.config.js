9c957adc2d5e15113e5ff32b86deb2611443bad4
Ben Smithett <bensmithett@users.noreply.github.com>
2022-08-24 09:05:30 +1000

    Extract table of contents from MDX and add basic TableOfContents component

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,8 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import mdx from '@mdx-js/rollup'
 import rehypeSlug from 'rehype-slug'
+import rehypeExtractToc from '@stefanprobst/rehype-extract-toc'
+import rehypeExtractTocMdx from '@stefanprobst/rehype-extract-toc/mdx'
 import remarkGfm from 'remark-gfm'
 import imagePresetsPlugin from 'vite-plugin-image-presets'
 import imagePresetsConfig from './src/imagePresets'
@@ -9,7 +11,7 @@ import imagePresetsConfig from './src/imagePresets'
 export const plugins = [
   react(),
   mdx({
-    rehypePlugins: [rehypeSlug],
+    rehypePlugins: [rehypeSlug, rehypeExtractToc, rehypeExtractTocMdx],
     remarkPlugins: [remarkGfm],
     providerImportSource: '@mdx-js/react'
   }),
